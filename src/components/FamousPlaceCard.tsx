983325405ec625772335c599230388c325732a3a
dubeyaditya543 <fordevelopmentusecase@gmail.com>
2026-06-03 18:55:37 +0530

    fixed some bugs

--- src/components/FamousPlaceCard.tsx
+++ src/components/FamousPlaceCard.tsx
@@ -18,7 +18,7 @@ export const FamousPlaceCard = ({ place }: CardProps) => {
     <article className="group flex flex-col h-48 sm:flex-row rounded-xl overflow-hidden border border-zinc-700/50 bg-zinc-800/30 hover:bg-zinc-800/50 transition-all duration-300 shadow-lg hover:shadow-xl">
       <div className="w-full sm:w-48 md:w-64 shrink-0 aspect-video sm:aspect-square md:aspect-video overflow-hidden relative">
         {isPending ? (
-          <div className="p-12 h-full w-full flex justify-center bg-zinc-900 border border-zinc-800 rounded-2xl">
+          <div className="p-12 h-full items-center w-full flex justify-center bg-zinc-900 border border-zinc-800 rounded-2xl">
             <div className="w-5 h-5 border-2 border-zinc-700 border-t-zinc-400 rounded-full animate-spin"></div>
           </div>
         ) : (
--- src/hooks/useImage.ts
+++ src/hooks/useImage.ts
@@ -7,13 +7,13 @@ export const useImage = (placeName: string) => {
     queryKey: ["image", placeName],
     queryFn: async () => {
       const response = await fetch(
-        `https://api.unsplash.com/search/photos?query=${placeName}&client_id=${import.meta.env.VITE_UNSPLASH_KEY}&orientation=landscape`,
+        `https://api.unsplash.com/search/photos?query=${placeName}-${currentCity?.country}&client_id=${import.meta.env.VITE_UNSPLASH_KEY}&orientation=landscape`,
       );
       const data = await response.json();
-      console.log(data)
+      // console.log(data)
       return data?.["results"]?.[0]?.["urls"]?.["regular"] ?? "https://upload.wikimedia.org/wikipedia/commons/1/14/No_Image_Available.jpg?utm_source=commons.wikimedia.org&utm_campaign=index&utm_content=original";
     },
-    enabled: placeName !== null && placeName !== undefined && currentCity?.name !== placeName,
+    enabled: placeName !== null && placeName !== undefined && currentCity?.name !== placeName
   });
 
   return {data, isPending};
